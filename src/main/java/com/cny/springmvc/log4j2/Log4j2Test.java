faebacad6e899993a3941bc7a2611f3349488a5d
chennengyuan <1870680926@qq.com>
2023-07-24 21:57:12 +0800

    1

--- src/main/java/com/cny/springmvc/log4j2/Log4j2Test.java
+++ src/main/java/com/cny/springmvc/log4j2/Log4j2Test.java
@@ -1,20 +1,18 @@
 package com.cny.springmvc.log4j2;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author : chennengyuan
  */
+@Slf4j
 public class Log4j2Test {
 
     public static void main(String[] args) {
-        Logger log = LogManager.getLogger(Log4j2Test.class);
         log.info("info");
         log.debug("debug");
         log.warn("warn");
         log.error("error");
-        log.fatal("fatal");
         log.trace("trace");
     }
 
