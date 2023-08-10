9441c0132c671405e36d459d0548a58c57e25ccb
chennengyuan <1870680926@qq.com>
2023-08-10 19:41:46 +0800

    1

--- src/main/java/com/cny/springmvc/log4j2/Log4j2Test.java
+++ src/main/java/com/cny/springmvc/log4j2/Log4j2Test.java
@@ -2,6 +2,8 @@ package com.cny.springmvc.log4j2;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+
 /**
  * @author : chennengyuan
  */
@@ -14,6 +16,24 @@ public class Log4j2Test {
         log.warn("warn");
         log.error("error");
         log.trace("trace");
+
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        HashMap<Character,Integer> map = new HashMap<Character, Integer>();
+        int left=0;
+        int max=0;
+        for (int i = 0; i < s.length(); i++) {
+            if(map.containsKey(s.charAt(i))){
+                left=Math.max(left,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max=Math.max(max,i-left+1);
+        }
+        return max;
     }
 
 
